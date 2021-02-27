af1927446d5daf787bac944985a129f85da81ca8
kelzerock <kelzzzer@gmail.com>
2021-02-27 23:35:03 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array===undefined){return 0}
+  if(array.length===0){return 0}
+  // if(array===NaN){return 0}
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  array.sort(compareNumeric)
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined){return 0}
+  if(array.length===0){return 0}
+  // if(array===NaN){return 0}
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  array.sort(compareNumeric)
+  return array[array.length-1]
+  ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if(array===undefined){return 0}
+  if(array.length===0){return 0}
+  // if(array===NaN){return 0}
+  for(let i = 0; i<array.length; i++)
+  {result +=array[i]}
+  return result/(+array.length);
 }
